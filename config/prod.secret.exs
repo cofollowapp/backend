1964127323f148e5bcd4e91df03e346235836926
Josh Hamilton <nearbycoder@gmail.com>
2018-08-12 14:20:13 -0500

    test

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -1,7 +1,7 @@
 config :cofollow, CofollowWeb.Endpoint, secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure your database
-config :cofollow, CofollowWeb.Repo,
+config :cofollow, Cofollow.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: 20
