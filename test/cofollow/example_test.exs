fed1bc61f70bf6453c63d23019be4779b26e63b0
Josh Hamilton <nearbycoder@gmail.com>
2018-08-12 23:47:44 -0500

    testing

--- test/cofollow/example_test.exs
+++ test/cofollow/example_test.exs
@@ -5,9 +5,4 @@ defmodule Cofollow.ExampleTest do
     assert {:ok, %{id: id}} = {:ok, %{id: 21}}
     assert id == 21
   end
-
-  describe "just an example test" do
-    assert {:ok, %{id: id}} = {:ok, %{id: 24}}
-    assert id == 24
-  end
 end
